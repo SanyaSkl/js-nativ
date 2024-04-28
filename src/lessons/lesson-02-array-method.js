471199c793ec61413291ade3cbc10e14c66d4c18
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-04-28 22:54:15 +0300

    pop()

--- src/lessons/lesson-02-array-method.js
+++ src/lessons/lesson-02-array-method.js
@@ -1,9 +1,31 @@
-// push(), pop(), shift(), unshift() - мутирующие (изменяют исходный массив)
+// push(), pop(), shift(), unshift(), reverse() - мутирующие (изменяют исходный массив)
 
 //push() - добавляет 1 или более элементов в конец массива
 
-const names = ['Alex', 'Bob', 'Ivan', 'Sasha'];
-const result = names.push('Mark', 'Anna', 'Olga');
+// const names = ['Alex', 'Bob', 'Ivan', 'Sasha'];
+// const result = names.push('Mark', 'Anna', 'Olga');
+//
+// console.log("names:", names);  // names: ['Alex', 'Bob', 'Ivan', 'Sasha', 'Mark', 'Anna', 'Olga']
+// console.log("result:", result) // result: 7
 
-console.log("names:", names);  // names: ['Alex', 'Bob', 'Ivan', 'Sasha', 'Mark', 'Anna', 'Olga']
-console.log("result:", result) // result: 7
+//pop() - удаляет последний элемент массива и возвращает его
+
+// const names = ['Alex', 'Bob', 'Ivan', 'Sasha']; // ['Alex', 'Bob', 'Ivan']
+// const poppedResult = names.pop(); // poppedResult: Sasha
+//
+// console.log("names: ", names);
+// console.log("poppedResult: ", poppedResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
