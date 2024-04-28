c9ac6a4d36280daeb140aff4b9fa306ca4cc4db7
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-04-28 22:54:59 +0300

    shift()

--- src/lessons/lesson-02-array-method.js
+++ src/lessons/lesson-02-array-method.js
@@ -16,7 +16,20 @@
 // console.log("names: ", names);
 // console.log("poppedResult: ", poppedResult);
 
+// shift() - удаляет первый элемент массива и возвращает его
 
+// const names = ['Alex', 'Bob', 'Ivan', 'Sasha']; // ['Bob', 'Ivan', 'Sasha']
+// const shiftResult = names.shift(); // Alex
+//
+// console.log("names: ", names);
+// console.log("shiftResult: ", shiftResult);
+
+
+////////////// при использовании методов pop() и shift() на пустом массиве они возвращают undefined////////////////
+// const names = []
+// const result = names.pop // undefined
+// const result = names.shift // undefined
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
